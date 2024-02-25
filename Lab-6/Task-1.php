e89cf62d3255fb4d8c7e9095e64f2c306f7c7314
Jubaer Bin Murshed <157951851+Jubaer2003@users.noreply.github.com>
2024-02-25 16:30:35 +0600

    Add files via upload

---
+++ Lab-6/Task-1.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>Task_1.A_PUBLIC_HOME</title>
+    </head>
+    <body>
+        <div>
+            <form action="task-2.php">
+                <Table border = "1" style = "width:500;">
+                    <tr>
+                        <tr style:"hight:50;width:500;">
+                            <th align = "left">X Company<a floot = "right" href = "home.php">Home</a>|<a href = "login.php">Login</a>|<a href = "registration.php">Registration</th>
+                        </tr>
+                        <tr style:"hight:200;width:500;">
+                            <th>Welcome To X Company</th>
+                        </tr>
+                        <td style:"hight:40;width:500;">
+                            <th>Copyright @ 2017</th>
+                        </td>
+                </Table>
+            </Form>
+        </div>
+    </body>
+</html>
