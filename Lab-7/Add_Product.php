447518282dfecebc5dfcba448799650d09d59936
Jubaer Bin Murshed <157951851+Jubaer2003@users.noreply.github.com>
2024-03-07 16:32:50 +0600

    Add files via upload

---
+++ Lab-7/Add_Product.php
@@ -0,0 +1,20 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Name and Submit Button</title>
+</head>
+<body>
+    <form action="#" method="POST">
+        <label for="textbox">Name</label><br>
+        <input type="text" id="textbox" name="textbox"><br>
+        <label for="textbox">Buying Price</label><br>
+        <input type="text" id="textbox" name="textbox"><br>
+        <label for="textbox">Selling Price</label><br>
+        <input type="text" id="textbox" name="textbox"><br><hr>
+        <input type="checkbox" id="display" name="display" value="Display">
+        <label for="display"> Display</label><br><hr>
+        <button type="save">Save</button>
+    </form>
+</body>
+</html>
