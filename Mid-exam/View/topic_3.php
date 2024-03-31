cdfb093c8eee4c289745d8c1a8dac92a49c5bb36
Jubaer Bin Murshed <157951851+Jubaer2003@users.noreply.github.com>
2024-03-31 15:38:28 +0600

    Add files via upload

---
+++ Mid-exam/View/topic_3.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About | A B </title>
+</head>
+<body>
+    <header>
+        <h1>About Me</h1>
+        <nav>
+            <ul>
+                <li><a href="topic_3_index.html">Home</a></li>
+                <li><a href="topic_3_portfolio.html">Portfolio</a></li>
+                <li><a href="topic_3_contact.html">Contact</a></li>
+                <li><a href="topic_3_photos.html">Photos</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <h2>Personal Details</h2>
+        <p>Hi.. I am Deep...</p>
+    </main>
+
+    <footer>
+        <p>&copy; 2024 A B</p>
+    </footer>
+</body>
+</html>
